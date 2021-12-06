884aca072ff06164c26f172a33ae6f8d6799f9e5
syuumy5050 <osamigaki3856@gmail.com>
2021-12-06 11:55:15 +0900

    add coment

--- henge.py
+++ henge.py
@@ -18,8 +18,8 @@ def read_iom(model):
 
 def main():
     # モデルの読み込み
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s6', pretrained=True)   #ネット上のモデル
-    # model = torch.hub.load('.', 'custom', path='pt/yolov5s6', source='local') #ローカルのモデル
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s6', pretrained=True)   # ネット上のモデル
+    # model = torch.hub.load('.', 'custom', path='pt/yolov5s6', source='local') # ローカルのモデル
 
     # モデルと画像を対応付け
     iom_datas = read_iom(model)
@@ -32,15 +32,16 @@ def main():
     cap.set(cv2.CAP_PROP_FPS, 30)
     ret, frame = cap.read()
 
-    #動画処理 & 動画出力
+    # 動画処理 & 動画出力
     while cap.isOpened():
         if ret:
-            frame_mixed = frame.copy()
-            frame_mixed = cv2.flip(frame_mixed, 1)
+            frame_mixed = frame.copy()  # 加工用のフレームを作成
+            frame_mixed = cv2.flip(frame_mixed, 1) # 左右反転
             results = model(frame_mixed)  # 画像パスを設定し、物体検出を行う
             objects = results.xyxy[0].numpy()  # 検出結果を取得
             # print(objects)
 
+            # 検出したオブジェクト毎に画像を重ねていく
             for object in objects:
                 model_id = int(object[5])
                 if model_id in iom_datas:
@@ -48,8 +49,8 @@ def main():
                     # owi_data.print_object()
                     frame_mixed = owi_data.mix_image(frame_mixed, 1.0)
 
-            # frame_mixed = cv2.resize(frame_mixed, (1920, 1080))
             # cv2.imshow("source", frame)  # 処理前の映像表示
+            # frame_mixed = cv2.resize(frame_mixed, (1920, 1080))　# 出力サイズ変更
             cv2.imshow("mixed_iamge", frame_mixed)  # 処理後の映像表示
 
         if cv2.waitKey(DELAY) & 0xFF == ord('q'):
